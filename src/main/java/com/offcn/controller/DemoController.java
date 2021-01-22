5bd9f5fa370808afba1c053db44a261db34ac8bf
dsy <dsy@qq.com>
2021-01-22 11:19:22 +0800

    master

---
+++ src/main/java/com/offcn/controller/DemoController.java
@@ -0,0 +1,12 @@
+package com.offcn.controller;
+
+public class DemoController {
+
+    public void demo(){
+        System.out.println("this is master demo");
+    }
+
+    public void test(){
+        System.out.println("this is master test");
+    }
+}
